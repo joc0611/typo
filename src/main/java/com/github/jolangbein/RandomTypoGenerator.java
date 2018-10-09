ff781c9d0e891ac7d3ff88aba318f421c6a6387e
Jan Löbel <jan.loebel@logbuch.xyz>
2018-10-09 10:26:30 +0200

    Assured that a typo is guaranteed

--- src/main/java/com/github/jolangbein/RandomTypoGenerator.java
+++ src/main/java/com/github/jolangbein/RandomTypoGenerator.java
@@ -1,26 +1,35 @@
 package com.github.jolangbein;
 
-import org.springframework.stereotype.Component;
-
 import java.security.SecureRandom;
 import java.util.Random;
+
+import org.springframework.stereotype.Component;
 
 @Component
 public class RandomTypoGenerator
 {
-    private static final int HIGHEST_ASCII_CHAR = 126;
-    private static final int LOWEST_ASCI_CHAR = 33;
+	private static final int HIGHEST_ASCII_CHAR = 126;
+	private static final int LOWEST_ASCI_CHAR = 33;
 
-    private final Random random = new SecureRandom();
+	private final Random random = new SecureRandom();
 
-    public String getTypo(final String input)
-    {
-        final int typoIndex = random.nextInt(input.length());
-        return input.substring(0, typoIndex) + randomCharacter() + input.substring(typoIndex + 1);
-    }
+	public String getTypo(final String input)
+	{
+		final int typoIndex = random.nextInt(input.length());
+		final char randomCharacter = randomCharacter();
 
-    private char randomCharacter()
-    {
-        return (char) (random.nextInt(HIGHEST_ASCII_CHAR - LOWEST_ASCI_CHAR) + LOWEST_ASCI_CHAR);
-    }
+		// If the random character is already the same as the one on the random index ->
+		// try again.
+		if (randomCharacter == input.charAt(typoIndex))
+		{
+			return getTypo(input);
+		}
+
+		return input.substring(0, typoIndex) + randomCharacter + input.substring(typoIndex + 1);
+	}
+
+	private char randomCharacter()
+	{
+		return (char) (random.nextInt(HIGHEST_ASCII_CHAR - LOWEST_ASCI_CHAR) + LOWEST_ASCI_CHAR);
+	}
 }
