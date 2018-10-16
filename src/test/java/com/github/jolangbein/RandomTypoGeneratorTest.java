45bfc9ed00c84df1adbbecb5bdb832e7ee965dd5
Matthias Nemeth <manedev79@gmail.com>
2018-10-16 23:17:44 +0200

    Fix formatting and add newline at the end of file.

--- src/test/java/com/github/jolangbein/RandomTypoGeneratorTest.java
+++ src/test/java/com/github/jolangbein/RandomTypoGeneratorTest.java
@@ -14,33 +14,33 @@ import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RandomTypoGeneratorTest {
-    private static final String INPUT_STRING = "Joc rulez";
-    private static final String TYPO_STRING = "Joe rulez";
+	private static final String INPUT_STRING = "Joc rulez";
+	private static final String TYPO_STRING = "Joe rulez";
 	private static final int RANGE_OF_ASCII_CHARS = 126 - 33;
 	private static final int RANDOM_FOR_ASCII_LOWERCASE_E = 101 - 33;
 	private static final int RANDOM_FOR_ASCII_LOWERCASE_C = 99 - 33;
 
-    @Mock
-    private Random random;
+	@Mock
+	private Random random;
 
-    @InjectMocks
-    private RandomTypoGenerator randomTypoGenerator;
+	@InjectMocks
+	private RandomTypoGenerator randomTypoGenerator;
 
-    @Test
-    public void generatesTypo() {
-        when(random.nextInt(INPUT_STRING.length())).thenReturn(2);
-        when(random.nextInt(RANGE_OF_ASCII_CHARS)).thenReturn(RANDOM_FOR_ASCII_LOWERCASE_E);
+	@Test
+	public void generatesTypo() {
+		when(random.nextInt(INPUT_STRING.length())).thenReturn(2);
+		when(random.nextInt(RANGE_OF_ASCII_CHARS)).thenReturn(RANDOM_FOR_ASCII_LOWERCASE_E);
 
-        assertThat(randomTypoGenerator.getTypo(INPUT_STRING), equalTo(TYPO_STRING));
-    }
+		assertThat(randomTypoGenerator.getTypo(INPUT_STRING), equalTo(TYPO_STRING));
+	}
 
-    @Test
-    public void typoIsGuaranteed() {
+	@Test
+	public void typoIsGuaranteed() {
 		when(random.nextInt(INPUT_STRING.length())).thenReturn(2);
 		when(random.nextInt(RANGE_OF_ASCII_CHARS))
 				.thenReturn(RANDOM_FOR_ASCII_LOWERCASE_C)
 				.thenReturn(RANDOM_FOR_ASCII_LOWERCASE_E);
 
 		assertThat(randomTypoGenerator.getTypo(INPUT_STRING), equalTo(TYPO_STRING));
-    }
+	}
 }
